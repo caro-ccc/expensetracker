4c83826ef93d217cf35c82fcdccfd7a698b834ba
caro-ccc <caro19852722@gmail.com>
2024-09-21 18:52:41 -0600

    first commit

---
+++ src/components/AddTransaction.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    <h3>Agregar nueva transaccion </h3>
+    <form id="form">
+        <div class="form-contol">
+            <label for="text">Text</label>
+            <input type="text" id="text" 
+            placeholder="Enter text...">
+            
+        </div>
+        <div class="form-control">
+            <label for="amount">Cantidad <br>
+            (negative - expense, positive- income)</label>
+            <input type= "number" id="amount" placeholder="Ingrese cantidad">
+        </div>
+        <button class="btn">Agregar</button>
+    </form>
+</div>
+</template>
---
+++ src/components/TransactionList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    <h3>HISTORIAL</h3>
+    <ul id="list" class="list">
+        <li v-for="transaction in transactions" 
+        v-bind:key="transaction.id">
+        {{ transaction.text }}
+        <span>-${{ transaction.amount }}</span>
+        <button class="delete-btn">X</button>
+    </li>
+    <button class="delete-btn"></button>
+        
+        
+    </ul>
+</div>
+</template>
+<script setup>
+import {defineProps} from 'vue';
+const props = defineProps({
+    trasicationes: {
+        type: Array,
+        required: true,
+    }
+})
+</script>
+
+
+
+
